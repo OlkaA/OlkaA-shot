f6641560657a1b4a9bbbf7f799046c0d0d1a0f0c
Olha Afanasieva <afanasievaolja@gmail.com>
2020-10-04 15:04:13 +0200

    select in all directions

--- components/Product.jsx
+++ components/Product.jsx
@@ -9,9 +9,9 @@ const Product = (props) => {
   const [resize, setResize] = useState({ s: false, direction: "" });
   const [list, setList] = useState([]);
 
-  let shot = useRef(null);
-  let productWrapper = useRef(null);
-  let seResize = useRef(null);
+  const shot = useRef(null);
+  const productWrapper = useRef(null);
+  const seResize = useRef(null);
 
   const image = {
     url: props.imageUrl,
@@ -26,10 +26,8 @@ const Product = (props) => {
     SW: "sw",
   };
 
-
   const onMouseDown = (e) => {
     e.persist();
-
     if (
       e.pageX > shot.current?.getBoundingClientRect().x &&
       e.pageX < shot.current?.getBoundingClientRect().right &&
@@ -38,32 +36,44 @@ const Product = (props) => {
     ) {
       setDrawing(false);
     } else {
-      setSize({width: 0, height: 0})  
+      setSize({ width: 0, height: 0 });
       setDrawing(true);
+      let x, y;
+
+      x = e.clientX - productWrapper.current.getBoundingClientRect().x;
+
+      y = e.clientY - productWrapper.current.getBoundingClientRect().y;
       setPosition({
-        x:
-          e.clientX -
-          productWrapper.current.getBoundingClientRect().x,
-        y:
-          e.clientY -
-          productWrapper.current.getBoundingClientRect().y,
+        x: x,
+        y: y,
       });
     }
   };
 
   const onMouseMove = (e) => {
-    e.persist();   
+    e.persist();
 
     if (drawing === true) {
       e.preventDefault();
       setResize({ s: false, direction: "" });
-      const temporarySize =
-        e.clientX -
-        position.x -
-        productWrapper.current.getBoundingClientRect().x;
 
-      setSize({ width: temporarySize, height: temporarySize });
-    }  
+      const calcWidth =
+        e.clientX -
+        productWrapper.current.getBoundingClientRect().x -
+        position.x;
+
+      const calcHeight =
+        e.clientY -
+        productWrapper.current.getBoundingClientRect().y -
+        position.y;
+      
+      const finalHeight = calcHeight > 0 ? Math.abs(calcWidth) : - Math.abs(calcWidth);
+
+      setSize({
+        width: calcWidth,
+        height: finalHeight,
+      });
+    }
     if (resize.s === true) {
       e.preventDefault();
       e.stopPropagation();
@@ -82,9 +92,9 @@ const Product = (props) => {
 
   const onMouseUp = (e) => {
     e.persist();
-    e.preventDefault();    
+    e.preventDefault();
 
-    if (drawing && (size.width > 0 || size.height > 0)) {
+    if (drawing && (Math.abs(size.width) > 50 || Math.abs(size.height) > 50)) {
       const listItem = {
         top: position.y,
         left: position.x,
@@ -92,7 +102,6 @@ const Product = (props) => {
         height: size.height,
       };
       setList((list) => [...list, listItem]);
-      
     }
     if (resize.s) {
       const listItem = {
@@ -110,7 +119,7 @@ const Product = (props) => {
     setResize({ s: false, direction: "" });
   };
 
-  const onDragEnd = (e) => {  
+  const onDragEnd = (e) => {
     if (!resize.s) {
       let x =
         e.clientX -
@@ -158,6 +167,59 @@ const Product = (props) => {
     }
   };
 
+  const Shot = () => {
+    let top, left;
+
+    if (size.width > 0 && size.height > 0) {
+      left = position.x
+      top = position.y;      
+    }
+    if (size.width < 0 && size.height > 0) {
+      left = position.x + size.width;
+      top = position.y;     
+    }
+    if (size.width > 0 && size.height < 0) {
+      left = position.x
+      top = position.y + size.height;     
+    }
+    if (size.width < 0 && size.height < 0) {
+      left = position.x + size.width;
+      top = position.y + size.height;
+    } 
+
+    return (
+      <div
+        ref={shot}
+        className={styles.square}
+        style={{
+          left: left + "px", // X COORDINATE
+          top: top + "px", // Y COORDINATE
+          width: Math.abs(size.width) + "px",
+          height: Math.abs(size.height) + "px",
+          backgroundImage: `url('/${image.url}')`,
+          backgroundPosition: `${-left - 2}px ${-top - 2}px`,
+          backgroundSize: `${image.width + 13}px ${image.height}px`,
+        }}
+        draggable
+        onDragEnd={onDragEnd}
+      >
+        <div
+          style={{
+            width: size.width * 0.3 + "px",
+            height: size.height * 0.3 + "px",
+          }}
+          ref={seResize}
+          className={styles.seResize}
+          onMouseMove={onMouseMove}
+          onMouseDown={() =>
+            setResize({ s: true, direction: directionsEnum.SE })
+          }
+          onMouseUp={onMouseUp}
+        ></div>
+      </div>
+    );
+  };
+
   return (
     <div className={styles.product}>
       <div
@@ -169,35 +231,7 @@ const Product = (props) => {
         onMouseMove={onMouseMove}
       >
         <div className={styles.background}></div>
-        {list.length > 0 && (
-          <div
-            ref={shot}
-            className={styles.square}
-            style={{
-              top: position.y + "px",
-              left: position.x + "px",
-              width: size.width + "px",
-              height: size.height + "px",
-              backgroundImage: `url('/${image.url}')`,
-              backgroundPosition: `${-position.x - 2}px ${-position.y - 2}px`,
-              backgroundSize: `${image.width + 13}px ${image.height}px`,
-            }}
-            draggable
-            onDragEnd={onDragEnd}
-          >
-            <div
-              style={{
-                width: size.width * 0.3 + "px",
-                height: size.height * 0.3 + "px",
-              }}
-              ref={seResize}
-              className={styles.seResize}
-              onMouseMove={onMouseMove}
-              onMouseDown={() => setResize({ s: true, direction: directionsEnum.SE })}
-              onMouseUp={onMouseUp}
-            ></div>
-          </div>
-        )}
+        <Shot />
       </div>
       <ListSelections list={list} image={image} />
     </div>
